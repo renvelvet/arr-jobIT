2de464e9871ce8c5c4d5c30e19df8b163d7d2b6f
Resha Puspita <reshapuspita734@gmail.com>
2020-06-12 22:22:43 +0700

    debug comma

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-const jobIT = ["Front End Developer", 
-                "Back End Developer", 
-                "UI Designer",
-                "UX Researcher",
-                "Database Administrator",
-                "Android Developer",
-                "Cloud Engineer" ]
+const jobIT = [
+  "Front End Developer",
+  "Back End Developer",
+  "UI Designer",
+  "UX Researcher",
+  "Database Administrator",
+  "Android Developer",
+  "Cloud Engineer",
+];
 
 console.log(jobIT.length);
 console.log(jobIT);
 
-document.getElementById("headline").innerHTML = `<h2>${jobIT.length} Pekerjaan di Bidang IT:`
+document.getElementById(
+  "headline"
+).innerHTML = `<h2>${jobIT.length} Pekerjaan di Bidang IT:`;
 
-let jobArr = jobIT.map((arr) => `<li>${arr}</li>`)
-
-document.getElementById("list-pekerjaan").innerHTML = jobArr
-
-
+let jobArr = "";
+jobIT.forEach((arr) => {
+  jobArr = jobArr.concat(`<li>${arr}</li>`);
+});
+document.getElementById("list-pekerjaan").innerHTML = jobArr;
