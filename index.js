db99a1e941791f7a4b093fb2c784dbf52b895151
Resha Puspita <reshapuspita734@gmail.com>
2020-06-12 16:54:18 +0700

    add files

---
+++ index.js
@@ -0,0 +1,18 @@
+const jobIT = ["Front End Developer", 
+                "Back End Developer", 
+                "UI Designer",
+                "UX Researcher",
+                "Database Administrator",
+                "Android Developer",
+                "Cloud Engineer" ]
+
+console.log(jobIT.length);
+console.log(jobIT);
+
+document.getElementById("headline").innerHTML = `<h2>${jobIT.length} Pekerjaan di Bidang IT:`
+
+let jobArr = jobIT.map((arr) => `<li>${arr}</li>`)
+
+document.getElementById("list-pekerjaan").innerHTML = jobArr
+
+
